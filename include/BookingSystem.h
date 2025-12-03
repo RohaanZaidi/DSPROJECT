b5750426dbd9c8da83b7f8c2bbf9818ac298ffe8
hassan28964 <K242033@nu.edu.pk>
2025-12-03 12:49:35 +0500

    Update BookingSystem.h

--- include/BookingSystem.h
+++ include/BookingSystem.h
@@ -1,27 +1,118 @@
-#ifndef BOOKING_SYSTEM_H
-#define BOOKING_SYSTEM_H
+#ifndef SMART_CAB_SYSTEM_H
+#define SMART_CAB_SYSTEM_H
 
-#include <queue>
-#include <vector>
-#include "CabManagement.h"
-#include "TripHistory.h"
-#include "FareCalculator.h"
+//------------------------------------------------------------
+// RIDER CLASS
+//------------------------------------------------------------
+class Rider {
+public:
+    int RiderID;
+    int PickupLocation;
 
-struct BookingRequest {
-    string riderName;
-    double riderX, riderY;
+    Rider();
+    Rider(int riderID, int pickup);
 };
 
-class BookingSystem {
+//------------------------------------------------------------
+// RIDER QUEUE (ARRAY-BASED)
+//------------------------------------------------------------
+class RiderQueue {
+public:
+    Rider arr[50];
+    int front;
+    int rear;
+
+    RiderQueue();
+
+    bool isFULL();
+    bool isEmpty();
+
+    void insert(int riderID, int pickupLocation);
+    void dequeue();
+};
+
+//------------------------------------------------------------
+// DRIVER CLASS
+//------------------------------------------------------------
+class Driver {
+public:
+    int id;
+    char name[20];
+    int location;
+    bool available;
+
+    Driver();
+};
+
+//------------------------------------------------------------
+// MIN-HEAP PRIORITY QUEUE FOR NEAREST DRIVER
+//------------------------------------------------------------
+class DriverMinHeap {
 private:
-    CabManagement &cabManager;
-    TripHistory &tripHistory;
-    FareCalculator &fareCalc;
+    Driver* heap[50];
+    int distanceArr[50];
+    int size;
+
+    void heapifyUp(int index);
+    void heapifyDown(int index);
 
 public:
-    BookingSystem(CabManagement &c, TripHistory &t, FareCalculator &f);
+    DriverMinHeap();
 
-    string requestCab(const string& riderName, double x, double y);
+    bool isEmpty();
+    void push(Driver* d, int riderPickup);
+    Driver* pop();
+};
+
+//------------------------------------------------------------
+// DRIVER HASH MAP (LINEAR PROBING)
+//------------------------------------------------------------
+#define TABLE_SIZE 50
+
+class DriverHashMap {
+private:
+    Driver* table[TABLE_SIZE];
+
+public:
+    DriverHashMap();
+
+    int hashFunction(int key);
+    void insertDriver(Driver* d);
+    Driver* getDriver(int driverID);
+    void setAvailability(int driverID, bool status);
+    void printDrivers();
+};
+
+//------------------------------------------------------------
+// TRIP CLASS
+//------------------------------------------------------------
+class Trip {
+public:
+    int tripID;
+    int riderID;
+    int driverID;
+    int pickup;
+    int drop;
+
+    Trip* next;
+
+    Trip(int tID, int rID, int dID, int pick, int drp);
+};
+
+//------------------------------------------------------------
+// LINKED LIST FOR ACTIVE TRIPS
+//------------------------------------------------------------
+class TripList {
+private:
+    Trip* head;
+
+public:
+    TripList();
+
+    void addTrip(int tripID, int riderID, int driverID, int pickup, int drop);
+    void completeTrip(int tripID);
+    void displayTrips();
 };
 
 #endif
+
